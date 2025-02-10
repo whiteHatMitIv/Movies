f3d69109055c153115afe5e2ff88ba64ebf82699
Ivan <ivannteumi@gmail.com>
2025-02-10 02:42:14 +0100

    `Update axiosConfig.js to keep the same baseURL`

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL:'http://localhost:8080',
+    baseURL:'http://localhost:8080', //adresse du serveur Spring boot en local
     headers: {"ngrok-skip-browser-warning": "true"}
 });
